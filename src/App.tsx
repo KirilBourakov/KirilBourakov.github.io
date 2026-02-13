dafe12743f36b08d903313f193b4839dbb1eec4c
Kiril <kirbou06012@gmail.com>
2026-02-12 20:23:38 -0400

    simplified app

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,20 @@
-import {Canvas, useThree} from '@react-three/fiber'
+import {Canvas} from '@react-three/fiber'
 import {Bloom, EffectComposer} from "@react-three/postprocessing";
 import MainLayout from "./components/index/MainLayout.tsx";
 
 
 export default function App() {
+    return (
+        <div style={{ height: '100vh', width: '100vw', margin: '0px' }}>
+            <Canvas dpr={[1, 1]}>
+                <EffectComposer autoClear={false} multisampling={0}>
+                    <Bloom luminanceThreshold={1} intensity={2} selectable />
+                </EffectComposer>
 
+                <MainLayout />
 
-  return (
-    <div style={{ height: '100vh', width: '100vw', margin: '0px' }}>
-      <Canvas dpr={[1, 1]}>
-          <EffectComposer autoClear={false} multisampling={0}>
-              <Bloom luminanceThreshold={1} intensity={2} selectable />
-          </EffectComposer>
-
-        <MainLayout />
-
-
-          {/*<ClippedPlanet />*/}
-
-
-
-          {/*<RotatingSun />*/}
-
-          {/*<BackPlanet />*/}
-
-          {/*<CenterShip />*/}
-
-          <ambientLight intensity={2} />
-      </Canvas>
-    </div>
-  )
+                <ambientLight intensity={2} />
+            </Canvas>
+        </div>
+    )
 }
