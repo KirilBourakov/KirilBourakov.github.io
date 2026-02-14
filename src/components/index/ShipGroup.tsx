cb92fa6530f55ce72351c7403cf41b7100f85d68
Kiril <kirbou06012@gmail.com>
2026-02-14 17:14:39 -0400

    simplified code

--- src/components/index/ShipGroup.tsx
+++ src/components/index/ShipGroup.tsx
@@ -18,20 +18,19 @@ export default function ShipGroup({ cameraRef }: {cameraRef: RefObject<CameraCon
     useCursor(hover && !isZoomed)
 
     function zoom() {
-        if (meshRef.current) {
-            if (zoomFocus == ZoomType.NONE) {
-                setZoomFocus(ZoomType.PROJECTS)
-                cameraRef.current.fitToBox(
-                    meshRef.current,
-                    true,
-                    {
-                        paddingLeft: -2,
-                        paddingRight: 0,
-                        paddingTop: 0,
-                        paddingBottom: 0
-                    }
-                );
-            }
+        if (meshRef.current && zoomFocus === ZoomType.NONE) {
+            setZoomFocus(ZoomType.PROJECTS)
+            cameraRef.current.fitToBox(
+                meshRef.current,
+                true,
+                {
+                    paddingLeft: -2,
+                    paddingRight: 0,
+                    paddingTop: 0,
+                    paddingBottom: 0
+                }
+            );
+
         }
     }
 
