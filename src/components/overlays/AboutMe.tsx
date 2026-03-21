4f536f172b4af69f5f482338d2dea11a23d3b326
Kiril <kirbou06012@gmail.com>
2026-03-20 21:49:01 -0300

    updated AboutMe.tsx

--- src/components/overlays/AboutMe.tsx
+++ src/components/overlays/AboutMe.tsx
@@ -1,11 +1,49 @@
+import {MdCheck, MdContentCopy} from "react-icons/md";
+import {useState} from "react";
+
 export default function AboutMe() {
+    const [isCopied, setIsCopied] = useState(false);
+    const email = "kirbou06012@gmail.com";
+
+    const handleCopy = async () => {
+        try {
+            await navigator.clipboard.writeText(email);
+            setIsCopied(true);
+            setTimeout(() => {
+                setIsCopied(false);
+            }, 1500);
+        } catch (err) {
+            console.error("Failed to copy text: ", err);
+        }
+    }
+
     return (
         <>
             <div
-                className="text-xl absolute top-0 right-0 bg-black/30 text-white w-1/4 p-2"
+                className="text-xl absolute top-0 right-0 bg-black/30 text-white w-1/4 p-2 pb-3"
                 style={{ clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 5% 100%)' }}
             >
-                <h1 className="font-bold ml-3">Kiril Bourakov</h1>
+                <div className="ml-6">
+                    <h1 className="font-bold">Kiril Bourakov</h1>
+                    <div className="flex items-center px-1">
+                        <h2 className="mr-1 leading-none">Email:</h2>
+                        <h2 className="mr-1 leading-none">{email}</h2>
+                        <div>
+                            <button
+                                className="hover:cursor-pointer transition-all hover:scale-110 translate-y-[2px] select-none"
+                                onClick={handleCopy}
+                            >
+                                {isCopied ? (
+                                    <MdCheck className="text-lg text-green-700" />
+                                ) : (
+                                    <MdContentCopy className="text-lg" />
+                                )}
+                            </button>
+                        </div>
+                    </div>
+                    <h2 className="px-1">Phone: (506) 997-0081</h2>
+                </div>
+
             </div>
         </>
     )
