fc4e4e4e7e8ee604f4ecb7c5f50fb709609fb466
Kiril <kirbou06012@gmail.com>
2026-03-29 20:12:07 -0300

    switched to environment for background

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import MainLayout from "./components/index/MainLayout.tsx";
 import {ZoomContextProvider} from "./hooks/ZoomContext.tsx";
 import OverlayManager from "./components/overlays/OverlayManager.tsx";
 import {useRef} from "react";
-import {CameraControls} from "@react-three/drei";
+import {CameraControls, Environment} from "@react-three/drei";
 
 export default function App() {
     const cameraRef = useRef<CameraControls>(null!)
@@ -14,6 +14,11 @@ export default function App() {
             <div className="w-screen h-screen m-0 relative overflow-hidden">
                 <div className="absolute right-0 top-0 w-full h-full">
                     <Canvas dpr={[1, 1]}>
+                        <Environment
+                            files="/bg.jpg"
+                            background
+                        />
+
                         <EffectComposer autoClear={false} multisampling={0}>
                             <Bloom luminanceThreshold={1} intensity={2} selectable />
                         </EffectComposer>
